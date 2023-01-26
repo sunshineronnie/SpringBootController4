9f51294f5e99a9beddb50cc9646072cadf8c3b88
Admin <kqadb@mail.ru>
2023-01-26 19:55:12 +0500

    Initial commit

---
+++ src/main/java/pro/sky/SpringBootController4/SpringBootController4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringBootController4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootController4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootController4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/SpringBootController4/SpringBootController4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringBootController4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootController4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
